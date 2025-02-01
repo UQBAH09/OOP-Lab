11c8fe77f32443be6bcaa904ee49ef00c5c296bb
UQBAH <126070473+UQBAH09@users.noreply.github.com>
2025-02-01 12:32:49 +0500

    Update Q1.cpp

--- Lab 1/Q1.cpp
+++ Lab 1/Q1.cpp
@@ -4,41 +4,62 @@ using namespace std;
 
 void argToFloat(char *argv[], float num[], int n)
 {
-	for (int i = 1; i <= n; i++)
-	{
-		num[i - 1] = atof(argv[i]);
-	}
+    for (int i = 1; i <= n; i++)
+    {
+        num[i - 1] = atof(argv[i]);
+    }
 }
 
-float secondHighest(float num[], int n)
+float secondHighest(float num[], int n, int *error)
 {
-	float temp;
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < n - i - 1; j++)
-		{
-			if (*(num + j) < *(num + j + 1))
-			{
-				temp = *(num + j);
-				*(num + j) = *(num + j + 1);
-				*(num + j + 1) = temp;
-			}
-		}
-	}
-	return num[1];
+    *error = 0;
+
+    float temp;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (*(num + j) < *(num + j + 1))
+            {
+                temp = *(num + j);
+                *(num + j) = *(num + j + 1);
+                *(num + j + 1) = temp;
+            }
+        }
+    }
+
+    int max = num[0];
+    for (int i = 0; i < n; i++)
+    {
+        if (num[i] < max)
+        {
+            return num[i];
+        }
+    }
+
+    cerr << "Error: All numbers are the same. No second highest number." << endl;
+    *error = 1;
+    return -1;
 }
 
 int main(int argc, char *argv[])
 {
-	if (argc < 3)
-	{
-		cout << "Too little arguments." << endl;
-		return 1;
-	}
+    if (argc < 3)
+    {
+        cout << "Too little arguments." << endl;
+        return 1;
+    }
 
-	int n = argc - 1;
-	float num[n];
+    int n = argc - 1;
+    float num[n];
+    int error;
 
-	argToFloat(argv, num, n);
-	cout << "The second higest number is " << secondHighest(num, n) << endl;
+    argToFloat(argv, num, n);
+
+    float number = secondHighest(num, n, &error);
+
+    if (!error)
+    {
+        cout << "The second higest number is " << number << endl;
+    }
 }
